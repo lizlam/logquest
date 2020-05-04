32046661f7ab0bb2b363a1c9cac5dff4280a6950
Liz Lam <liz_lam_217@yahoo.com>
2020-05-03 19:58:05 -0700

    Add fonts and grid styling.

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,11 +1,17 @@
 import image from 'web/public/images/rocket.png'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 50% 50%;
+`
 
 const HomePage = () => {
   return (
-    <div>
+    <Container>
       <img alt="Rocket image" src={image} />
       <h1>Log Quest</h1>
-    </div>
+    </Container>
   )
 }
 
